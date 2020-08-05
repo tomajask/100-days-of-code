a73d130fc7ec0b8d5522f8f58699cc1a53cb2674
Tomasz Jaśkiewicz <tomasz.jaskiewicz@netguru.com>
2020-08-05 08:53:08 +0200

    R1D37 - JS | Callback Abstraction

--- projects/js-bootcamp/hangman/app.js
+++ projects/js-bootcamp/hangman/app.js
@@ -23,35 +23,31 @@ window.addEventListener('keypress', (e) => {
   console.log(`Status: ${game1.status}`)
 })
 
-
-// Making an HTTP request
-const request = new XMLHttpRequest()
-
-request.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status == 200) {
-    const data = JSON.parse(e.target.responseText)
-    console.log(data.puzzle)
-  } else if (e.target.readyState === 4) {
-    console.log('An error has taken place')
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(puzzle)
   }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
 
-const countryCode = "US"
+// // Making an HTTP request
 
-const req = new XMLHttpRequest()
 
-req.addEventListener('readystatechange', (e) => {
-  if (e.target.status === 200 & e.target.readyState === 4) {
-    const data = JSON.parse(e.target.responseText)
-    const country = data.find((item) => item.alpha2Code === countryCode)
-    console.log(country.name)
-  } else if (e.target.readyState === 4) {
-    console.log('An error has taken place')
-  }
-})
+// const countryCode = "PL"
 
-req.open('GET', 'http://restcountries.eu/rest/v2/all')
-req.send()
+// const req = new XMLHttpRequest()
+
+// req.addEventListener('readystatechange', (e) => {
+//   if (e.target.status === 200 & e.target.readyState === 4) {
+//     const data = JSON.parse(e.target.responseText)
+//     const country = data.find((item) => item.alpha2Code === countryCode)
+//     console.log(country.name)
+//   } else if (e.target.readyState === 4) {
+//     console.log('An error has taken place')
+//   }
+// })
+
+// req.open('GET', 'http://restcountries.eu/rest/v2/all')
+// req.send()
--- r1-log.md
+++ r1-log.md
@@ -4,6 +4,10 @@ The log of my #100DaysOfCode challenge. Started on [June 16, 2020].
 
 ## Log
 
+### R1D37
+
+Continued with JS course -> Callback Abstraction.
+
 ### R1D36
 
 Continued with JS course -> fetching data from APIs.
